a169dfcb3ff0dbe4ca2a189ed902cfe382f585be
Mqhele Kingdom Cele <mqhele03@gmail.com>
2026-03-05 12:41:12 +0200

    Delete GeneratedCV.tsx

--- GeneratedCV.tsx
+++
@@ -1,9 +0,0 @@
-<PrintFallbackModal>
-  <Modal.Header>
-    <AmberWarningIcon />
-    <h2>Service Unavailable</h2>
-  </Modal.Header>
-  <Modal.Body>
-    <p>PDF generation is temporarily unavailable. Please download your document in DOCX format.</p>
-  </Modal.Body>
-</PrintFallbackModal>
